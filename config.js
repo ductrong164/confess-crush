2d9e2db5c1b1c829e6d05513e91c1b2b1ebc16de
ductrong164 <bolaductrong164@gmail.com>
2025-06-10 00:35:48 +0700

    Update config.js
    Signed-off-by: ductrong164 <bolaductrong164@gmail.com>

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Thì Tại Anh Trọng Đẹp Trai To Cu Nên Em Yêu Anh Rất Nhiều Hẹ Hẹ<33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tôi thương bà lắm á , đừng bỏ tôi nheeeer :<<<<< .',
+    messDesc: 'Tôi thương bà lắm á , đừng bỏ tôi nheeee:<<<<< .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
